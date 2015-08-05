2d9460a6dc07472b2b46716b5041cb1c81a7aa06
lifeng <lifeng632@163.com>
2015-08-05 10:19:13 +0800

    gulp 添加sass编译

--- lightbox/gulpfile.js
+++ lightbox/gulpfile.js
@@ -7,6 +7,9 @@ var uglify = require('gulp-uglify');
 // 获取 minify-css 模块（用于压缩 CSS）
 var minifyCSS = require('gulp-minify-css');
 
+//获取gulp-ruby-sass模块
+var sass = require('gulp-ruby-sass')
+
 // 压缩 js 文件
 // 在命令行使用 gulp script 启动此任务
 gulp.task('script', function() {
@@ -30,6 +33,16 @@ gulp.task('css', function () {
         .pipe(gulp.dest('dist/css'))
 })
 
+//编译sass
+//在命令行输入gulp-sass启动此任务
+gulp.task('sass',function(){
+    return sass('sass/')
+    .on('error',function(err){
+        console.error('Error!',err.message);
+    })
+    .pipe(gulp.dest('css/'))
+});
+
 
 // 在命令行使用 gulp auto 启动此任务
 gulp.task('auto', function () {
@@ -37,9 +50,13 @@ gulp.task('auto', function () {
     gulp.watch('js/*.js', ['script'])
     // 监听文件修改，当文件被修改则执行 css 任务
     gulp.watch('css/*.css', ['css'])
-})
+    // 监听文件修改，当文件被修改则执行 sass 任务
+    gulp.watch('sass/**/*.scss',['sass'])
+
+});
 
 // 使用 gulp.task('default') 定义默认任务
 // 在命令行使用 gulp 启动 script 任务和 auto 任务
 // 在命令行使用 gulp 启动 css 任务和 auto 任务
-gulp.task('default', ['css','script', 'auto'])
+// 在命令行使用 gulp 启动 sass 任务和 auto 任务
+gulp.task('default', ['sass','css','script', 'auto'])
